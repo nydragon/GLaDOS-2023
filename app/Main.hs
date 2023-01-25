dd4f5f92358e471dedac6ba270591f129435b319
Nydragon <6582485n@proton.me>
2023-01-25 14:04:41 +0100

    [Refactor] add default args by default to tokenizer

--- app/Main.hs
+++ app/Main.hs
@@ -1,29 +1,27 @@
 module Main where
 
+tokenize' :: String -> String -> [String]
+tokenize' [] str = []
+tokenize' (' ':xs) str
+        | length str == 0 = tokenize' xs ""
+        | otherwise = str : tokenize' xs ""
+tokenize' ('\n':xs) str
+        | length str == 0 = tokenize' xs ""
+        | otherwise = str : tokenize' xs ""
+tokenize' ('(':xs) str
+        | length str == 0 = "(" : tokenize' xs ""
+        | otherwise = str : tokenize' ('(':xs) ""
+tokenize' (')':xs) str
+        | length str == 0 = ")" : tokenize' xs ""
+        | otherwise = str : tokenize' (')':xs) ""
+tokenize' (x:xs) str = tokenize' xs (str <> [x])
 
-
-
-tokenize :: String -> String -> [String]
-tokenize [] str = []
-tokenize (' ':xs) str
-        | length str == 0 = tokenize xs ""
-        | otherwise = str : tokenize xs ""
-tokenize ('\n':xs) str
-        | length str == 0 = tokenize xs ""
-        | otherwise = str : tokenize xs ""
-tokenize ('(':xs) str
-        | length str == 0 = "(" : tokenize xs ""
-        | otherwise = str : tokenize ('(':xs) ""
-tokenize (')':xs) str
-        | length str == 0 = ")" : tokenize xs ""
-        | otherwise = str : tokenize (')':xs) ""
-tokenize (x:xs) str = tokenize xs (str <> [x])
-
-
+tokenize :: String -> [String]
+tokenize str = tokenize' str ""
 
 main :: IO ()
 main = do
-    let tokenizedcode = tokenize "(define x 2)\n\n( add    x 3 )" ""
+    let tokenizedcode = tokenize "(define x 2)\n\n( add    x 3 )"
     print tokenizedcode
     
     
