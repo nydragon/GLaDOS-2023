ade631037962cc38871c20c7d87fd5787c05c1f1
Nydragon <6582485n@proton.me>
2023-01-25 12:58:56 +0100

    [Feat] update readme

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,15 @@
 module Main where
 
+
+consume :: [Char] -> [String] -> [String]
+consume [] ys = ys
+consume xs ys = parser xs ys
+
+parser :: [Char] -> [String] -> [String]
+parser (x:[]) ys = consume [] [[x]] <> ys
+parser (')':xs) ys = consume xs [")"] <> ys
+parser (x:xs) ys = parser xs [[x]] <> ys
+
 main :: IO ()
 main = do
-    putStrLn "Hello"
+    putStrLn ((parser "(2 (3 3))" [""]))
