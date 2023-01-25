b570299168bac4ccb625c97a39edb4f675c3b920
Léon Sautour <leon@sautour.net>
2023-01-25 20:16:59 +0100

    [Feature] Implemented Token type in parsing. Now returns [Token].

--- app/Parsing/Lang.hs
+++ app/Parsing/Lang.hs
@@ -1,48 +1,67 @@
 module Parsing.Lang where
 
+import Text.Read
+import Data.Char
+
 -- ─── Tokenization ────────────────────────────────────────────────────────────────────────────────
 
 -- Token Datatype
-data Token = OpenScope
-        | CloseScope
-        | Number Integer
+data Token = OpenScope -- Opening parenthesis
+        | CloseScope -- Closing parenthesis
+        | Num Integer
         | Keyword String
-        | OpenSingle -- single quotes
-        | CloseSingle
-        | OpenDouble -- double quotes
-        | CloseDouble
+        | Negation -- Minus
+        deriving (Show)
+
+-- Parse token from string
+parseToken :: String -> Token
+parseToken "(" = OpenScope
+parseToken ")" = CloseScope
+parseToken "-" = Negation
+parseToken input
+        | isNum input == True = Num (read input :: Integer)
+        | otherwise = Keyword input
 
 -- Function that tokenizes string
 --
 -- Tokens are : ' ', '\n', '(', ')'
 -- Args are : Input -> Temp Str -> Output List
-tokenize' :: String -> String -> [String]
+tokenize' :: String -> String -> [Token]
 tokenize' [] str = []
 tokenize' (' ':xs) str
         | null str = tokenize' xs ""
-        | otherwise = str : tokenize' xs ""
+        | otherwise = (parseToken str) : tokenize' xs ""
 tokenize' ('\n':xs) str
         | null str = tokenize' xs ""
-        | otherwise = str : tokenize' xs ""
+        | otherwise = (parseToken str) : tokenize' xs ""
 tokenize' ('(':xs) str
-        | null str = "(" : tokenize' xs ""
-        | otherwise = str : tokenize' ('(':xs) ""
+        | null str = OpenScope : tokenize' xs ""
+        | otherwise = (parseToken str) : tokenize' ('(':xs) ""
 tokenize' (')':xs) str
-        | null str = ")" : tokenize' xs ""
-        | otherwise = str : tokenize' (')':xs) ""
+        | null str = CloseScope : tokenize' xs ""
+        | otherwise = (parseToken str) : tokenize' (')':xs) ""
+tokenize' ('-':xs) str
+        | null str = Negation : tokenize' xs ""
+        | otherwise = (parseToken str) : tokenize' ('-':xs) ""
 tokenize' (x:xs) str = tokenize' xs (str <> [x])
 
 -- Utility entry point function
-tokenize :: String -> [String]
+tokenize :: String -> [Token]
 tokenize str = tokenize' str ""
 
 -- Function to tokenize a given file
 --
 -- Args : path
-tokenizeFile :: String -> IO [String]
+tokenizeFile :: String -> IO [Token]
 tokenizeFile path = do
         -- Read file and Tokenize
         fileStr <- readFile path
 
         -- Return tokenization result
         return (tokenize fileStr)
+
+-- ─── Utilities ───────────────────────────────────────────────────────────────────────────────────
+
+isNum :: String -> Bool
+isNum [] = True
+isNum (x:xs) = if isDigit x then isNum xs else False
