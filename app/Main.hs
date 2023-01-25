2b03124a7475291fc5757ace313623df3ca1294b
Nydragon <6582485n@proton.me>
2023-01-25 13:25:12 +0100

    [Feature] WIP

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,23 @@
 module Main where
 
+
+
+
+tokenize :: [Char] -> String -> [String]
+tokenize ('(':xs) b
+        | length b == 0 = "(" : tokenize xs ""
+        | otherwise = b : tokenize ('(':xs) ""
+tokenize (')':xs) b = b: ")" : tokenize xs ""
+tokenize (' ':xs) b = b : tokenize xs ""
+tokenize [] b = []
+tokenize (x:xs) b = tokenize xs (b <> [x])
+
+
+
 main :: IO ()
 main = do
-    putStrLn "Hello"
+    let tokenizedcode = tokenize "(define x 2)\n(add x 3)" ""
+
+    print tokenizedcode
+    
+    
