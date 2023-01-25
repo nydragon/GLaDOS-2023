75c011221d49d493eb2d41de0a8911deea763544
Nydragon <6582485n@proton.me>
2023-01-25 13:44:50 +0100

    [Feat] tokenizer is working

--- app/Main.hs
+++ app/Main.hs
@@ -3,21 +3,27 @@ module Main where
 
 
 
-tokenize :: [Char] -> String -> [String]
+tokenize :: String -> String -> [String]
+tokenize [] b = []
+tokenize (' ':xs) b
+        | length b == 0 = tokenize xs ""
+        | otherwise = b : tokenize xs ""
+tokenize ('\n':xs) b
+        | length b == 0 = tokenize xs ""
+        | otherwise = b : tokenize xs ""
 tokenize ('(':xs) b
         | length b == 0 = "(" : tokenize xs ""
         | otherwise = b : tokenize ('(':xs) ""
-tokenize (')':xs) b = b: ")" : tokenize xs ""
-tokenize (' ':xs) b = b : tokenize xs ""
-tokenize [] b = []
+tokenize (')':xs) b
+        | length b == 0 = ")" : tokenize xs ""
+        | otherwise = b : tokenize (')':xs) ""
 tokenize (x:xs) b = tokenize xs (b <> [x])
 
 
 
 main :: IO ()
 main = do
-    let tokenizedcode = tokenize "(define x 2)\n(add x 3)" ""
-
+    let tokenizedcode = tokenize "(define x 2)\n\n( add    x 3 )" ""
     print tokenizedcode
     
     
