4584ab670a5dfb72ae412bf14014feb8eccb5450
Leon Sautour <leon@sautour.net>
2023-01-26 12:20:27 +0100

    [Feature] Added utility entry point function for getNextSep

--- app/Parsing.hs
+++ app/Parsing.hs
@@ -10,8 +10,12 @@ splitOn c arr = take nextSep arr : splitOn c (drop nextSep arr)
     where nextSep = getNextSep c arr 0 + 1 -- +1 because it's a character count
 
 -- Gets index of next occurence of separator
-getNextSep :: (Eq a) => a -> [a] -> Int -> Int
-getNextSep _ [] i = i - 1 -- -1 because will have been incremented in recursive call
-getNextSep t (x:xs) i
+getNextSep' :: (Eq a) => a -> [a] -> Int -> Int
+getNextSep' _ [] i = i - 1 -- -1 because will have been incremented in recursive call
+getNextSep' t (x:xs) i
         | t == x = i
-        | otherwise = getNextSep t xs (i + 1)
+        | otherwise = getNextSep' t xs (i + 1)
+
+-- Utility entry point function
+getNextSep :: (Eq a) => a -> [a]
+getNextSep sep ls = getNextSep' sep ls 0
