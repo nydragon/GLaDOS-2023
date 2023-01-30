94af655165fd6d91bd4b1aba397f5bc8835ef419
Leon Sautour <leon@sautour.net>
2023-01-30 11:28:03 +0100

    [Test] Added multi file tests

--- test/Test.hs
+++ test/Test.hs
@@ -3,12 +3,15 @@ import Test.Tasty
 import Test.Tasty.Discover
 import Test.Tasty.HUnit
 
+import TokenizationTest
+
 {-# OPTIONS_GHC -F -pgmF tasty-discover#-}
 
 main = defaultMain tests
 
 tests :: TestTree
-tests =testGroup "UnitTests"
+tests =testGroup "BABABABABABABABABABABABABABA"
     [testCase "first" $
-        2+2 @?= 4
+        2+2 @?= 4,
+    tokenSuite
     ]
