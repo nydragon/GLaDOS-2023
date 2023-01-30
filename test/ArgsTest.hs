7771198888189ae9712799118946ac403aafd920
Virgile Courtial <virgile.courtial@epitech.eu>
2023-01-30 15:34:37 +0100

    [Test] parsing test

---
+++ test/ArgsTest.hs
@@ -0,0 +1,17 @@
+module ArgsTest where
+
+import Parsing
+import Parsing.Args
+import Test.Tasty
+import Test.Tasty.HUnit
+
+parseArgsTest :: [String] -> ParsedArgs -> TestTree
+parseArgsTest arr restest = testCase "test parse args" $ do
+    res <- parseArgs' arr 0 (ParsedArgs False "")
+    restest @=? res
+
+argsSuite :: TestTree
+argsSuite = testGroup "Unit Tests" [
+        parseArgsTest ["", "try", ""] (ParsedArgs False "try"),
+        parseArgsTest ["", "test", "-h"] (ParsedArgs True "test")
+    ]
