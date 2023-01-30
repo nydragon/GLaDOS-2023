535fa1f61cfd4fa3e22a91a7fde9fee154083114
Leon Sautour <leon@sautour.net>
2023-01-30 19:20:49 +0100

    [Feature] WIP Expr datatype specification.

--- lib/Parsing/Ast.hs
+++ lib/Parsing/Ast.hs
@@ -2,6 +2,14 @@ module Parsing.Ast where
 
 -- ─── Abstract Syntaxe Tree ───────────────────────────────────────────────────────────────────────
 
-data Ast = Define String Ast
-    | Num Int
+data Expr = List [Expr]
+    | Num Integer
+    | Sym String
+    | Call String [Expr] -- Will also be used for the boolean expression
 
+-- DEFINITIONS
+    -- Expression = Anything that ends up a value
+    -- Function Call = When first element of list is symbol
+
+-- NOTES
+    -- DID NOT implement ternary (if else) statement for the time being
