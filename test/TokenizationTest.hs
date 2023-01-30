80773b4a8124b6fb6dbc3395ac0aad6d8c4c130b
Leon Sautour <leon@sautour.net>
2023-01-30 15:38:27 +0100

    [Test] Add parseToken and tokenize tests

--- test/TokenizationTest.hs
+++ test/TokenizationTest.hs
@@ -3,9 +3,42 @@ module TokenizationTest where
 import Test.Tasty
 import Test.Tasty.HUnit
 
-test1 :: TestTree
-test1 = testCase "Test 1" $ 2 + 2 @?= 4
+import Parsing.Lang
+
+-- ─── Parse Token Tests ───────────────────────────────────────────────────────────────────────────
+parseTokenTests :: TestTree
+parseTokenTests = testGroup "parseToken tests"
+  [ testCase "OpenScope" $ parseToken "(" @?= OpenScope
+  , testCase "CloseScope" $ parseToken ")" @?= CloseScope
+  , testCase "Num" $ parseToken "123" @?= Num 123
+  , testCase "Negative Num" $ parseToken "-123" @?= Num (-123)
+  , testCase "Keyword" $ parseToken "keyword" @?= Keyword "keyword"
+  ]
+
+tokenizeTests :: TestTree
+tokenizeTests = testGroup "tokenize tests"
+  [ testCase "tokenize' empty input" $
+      tokenize' "" "" @?= []
+  , testCase "tokenize' space" $
+      tokenize' " " "" @?= []
+  , testCase "tokenize' newline" $
+      tokenize' "\n" "" @?= []
+  , testCase "tokenize' single digit" $
+      tokenize' "1" "" @?= [Num 1]
+  , testCase "tokenize' multiple digits" $
+      tokenize' "123" "" @?= [Num 123]
+  , testCase "tokenize' keyword" $
+      tokenize' "keyword" "" @?= [Keyword "keyword"]
+  , testCase "tokenize' open scope" $
+      tokenize' "(" "" @?= [OpenScope]
+  , testCase "tokenize' close scope" $
+      tokenize' ")" "" @?= [CloseScope]
+  , testCase "tokenize' mixed input" $
+      tokenize' "1 keyword ( 123 )" "" @?= [Num 1, Keyword "keyword", OpenScope, Num 123, CloseScope]
+  , testCase "tokenize" $
+      tokenize "1 keyword ( 123 )" @?= [Num 1, Keyword "keyword", OpenScope, Num 123, CloseScope]
+  ]
 
 -- Test suite list
 tokenSuite :: TestTree
-tokenSuite = testGroup "Tokenization Test Suite" [test1]
+tokenSuite = testGroup "Tokenization Test Suite" [parseTokenTests]
