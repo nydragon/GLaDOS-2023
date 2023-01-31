e36dd627be2b6ddbb3ed30aec6c90b9e8ca3b399
Leon Sautour <leon@sautour.net>
2023-01-31 10:55:07 +0100

    [Feature] add function and var defines

--- app/Exec.hs
+++ app/Exec.hs
@@ -12,14 +12,38 @@ data Atom = Name String
 -- Utility type synonyms
 type VarLookup = Map String Atom -- Var name, var value
 type FuncLookup = Map String ExprList -- Function name, function body
+type Registry = (VarLookup, FuncLookup) -- The registry is just a tuple of two maps
 
 -- ─── Function And Var Lookup ─────────────────────────────────────────────────────────────────────
 
+-- Get definitions
 lookupVar :: String -> VarLookup -> Maybe Atom
 lookupVar name variables = lookup name variables
 
+-- Get definitions
 lookupFunc :: String -> FuncLookup -> Maybe ExprList
 lookupFunc name functions = lookup name functions
+
+-- To check if name is already defined
+isNameDefined :: String -> Registry -> Bool
+isNameDefined name (vars, funcs) = member name vars || member name funcs
+
+-- Defines a variable
+-- Returns Nothing if name is already used
+defineVar :: String -> Atom -> Registry -> Maybe Registry
+defineVar name val (vars, funcs) = if isNameDefined name reg then Nothing else Just newReg
+    where
+        reg = (vars, funcs)
+        newReg = (insert name val vars, funcs)
+
+-- Defines a function
+-- Returns Nothing if name is alreadt used
+defineFunc :: String -> ExprList -> Registry -> Maybe Registry
+defineFunc name def (vars, funcs) = if isNameDefined name reg then Nothing else Just newReg
+    where
+        reg = (vars, funcs)
+        newReg = (vars, insert name def funcs)
+
 
 -- ─── Main Function ───────────────────────────────────────────────────────────────────────────────
 
