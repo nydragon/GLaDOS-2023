7b91efb9de4dad0ad4a544bae5605ebac8dd366f
Nydragon <6582485n@proton.me>
2023-02-03 19:49:45 +0100

    [Feature] implement show instance for Exceptions, messages not final

--- lib/Exec/RuntimeException.hs
+++ lib/Exec/RuntimeException.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE InstanceSigs #-}
+
 module Exec.RuntimeException where
 
 import Control.Exception
@@ -6,13 +8,21 @@ import Exec.Lookup
 -- Runtime exception data type
 --
 -- NOTE: This definition will be completed as development continues
-data RuntimeException = InvalidFunctionCall String
-    | UndefinedBehaviour -- Pretty much for anything that shouldn't ever happen
-    | NotYetImplemented
-
-    -- args: argument index, expected type, got type
-    | InvalidArgument Integer String String
-    | NullDivision
-    deriving (Show, Eq)
+data RuntimeException
+  = InvalidFunctionCall String
+  | UndefinedBehaviour -- Pretty much for anything that shouldn't ever happen
+  | NotYetImplemented
+  | -- args: argument index, expected type, got type
+    InvalidArgument Integer String String
+  | NullDivision
+  deriving (Eq)
 
 instance Exception RuntimeException
+
+instance Show RuntimeException where
+  show :: RuntimeException -> String
+  show (InvalidFunctionCall a) = a ++ " is an invalid function call."
+  show UndefinedBehaviour = "This is undefined."
+  show NotYetImplemented = "OH MAXI BEBOU"
+  show (InvalidArgument a b c) = "Argument " ++ show a ++ " invalid: expected '" ++ b ++ "' but received '" ++ c ++ "'."
+  show NullDivision = "Did you not pay attention in math class?"
