96185f0bf3a5b17188e3210166e8b75099c237f0
Nydragon <6582485n@proton.me>
2023-02-05 18:26:11 +0100

    [Fix] remove unneeded dependecies

--- app/Main.hs
+++ app/Main.hs
@@ -1,47 +1,46 @@
 module Main where
 
 -- Our modules
-import Parsing
-import Parsing.Token
-import Parsing.Cpt
-import Parsing.Ast
-import Parsing.Args
-import System.Console.GetOpt
-import Exec
 
+import Data.Maybe (fromMaybe)
+import Exec
+import Exec.Eval
+import Exec.InteractivePrompt
+import Parsing
+import Parsing.Args
+import Parsing.Ast
+import Parsing.Cpt
+import Parsing.Token
+import System.Console.GetOpt
 import System.Environment
 import System.Exit
-import Data.Maybe (fromMaybe)
-import Exec.InteractivePrompt
 
 getFileName :: [String] -> Maybe FilePath -> String
 getFileName [] b = fromMaybe "stdin" b
-getFileName (x:xs) b = x
+getFileName (x : xs) b = x
 
 runFile :: String -> IO ()
 runFile filename = do
-    -- Tokenize
-    tokens <- tokenizeFile filename
+  -- Tokenize
+  tokens <- tokenizeFile filename
 
-    -- Parse CPT
-    let cpt = parseTokenList tokens
+  -- Parse CPT
+  let cpt = parseTokenList tokens
 
-    -- Parse AST
-    let ast = parseExprList cpt
+  -- Parse AST
+  let ast = parseExprList cpt
+  -- Run
+  -- run ast
 
-    -- Run
-    -- run ast
-
-    return ()
-
+  return ()
 
 main :: IO ()
-main = do
+main =
+  do
     -- Parsing arguments
     (res, fls) <- getArgs >>= parse
 
     let fileName = getFileName fls (file res)
-    print ("Execute file: " ++  fileName)
     if (==) fileName "stdin"
-        then interactiveMode -- interactive
-        else runFile fileName -- normal
+      then interactiveMode -- interactive
+      else runFile fileName -- normal
