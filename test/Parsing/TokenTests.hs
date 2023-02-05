15c4aa3da08e941724c0e301ec91984750b691c0
Virgile Courtial <virgile.courtial@epitech.eu>
2023-02-05 20:02:00 +0100

    [Style] clean unit test for tokenization

--- test/Parsing/TokenTests.hs
+++ test/Parsing/TokenTests.hs
@@ -5,6 +5,19 @@ import Test.Tasty.HUnit
 
 import Parsing.Token
 
+listTokenizeTests = [
+        ("empty input", "", []),
+        ("space", " ", []),
+        ("new line", "\n", []),
+        ("single digit", "1", [Num 1]),
+        ("mulitple digit", "123", [Num 123]),
+        ("single Keyword", "keyword", [Keyword "keyword"]),
+        ("open scope", "(", [OpenScope]),
+        ("close Scope", ")", [CloseScope]),
+        ("1 keyword ( 123 )", "1 keyword ( 123 )", [Num 1, Keyword "keyword", OpenScope, Num 123, CloseScope]),
+        ("(define x 2)\n(+ 3 x)", "(define x 2)\n(+ 3 x)", [OpenScope, Keyword "define", Keyword "x", Num 2, CloseScope, OpenScope, Keyword "+", Num 3, Keyword "x", CloseScope])
+    ]
+
 parseTokenTests :: TestTree
 parseTokenTests = testGroup "parseToken tests"
   [ testCase "OpenScope" $ parseToken "(" @?= OpenScope
@@ -14,34 +27,20 @@ parseTokenTests = testGroup "parseToken tests"
   , testCase "Keyword" $ parseToken "keyword" @?= Keyword "keyword"
   ]
 
-tokenizeTests :: TestTree
-tokenizeTests = testGroup "tokenize tests"
-  [ testCase "tokenize' empty input" $
-        tokenize' "" "" @?= [],
-    testCase "tokenize' space" $
-        tokenize' " " "" @?= [],
-    testCase "tokenize' newline" $
-        tokenize' "\n" "" @?= [],
-    testCase "tokenize' single digit" $
-        tokenize' "1" "" @?= [Num 1],
-    testCase "tokenize' multiple digits" $
-        tokenize' "123" "" @?= [Num 123],
-    testCase "tokenize' keyword" $
-        tokenize' "keyword" "" @?= [Keyword "keyword"],
-    testCase "tokenize' open scope" $
-        tokenize' "(" "" @?= [OpenScope],
-    testCase "tokenize' close scope" $
-        tokenize' ")" "" @?= [CloseScope],
-    testCase "tokenize' mixed input" $
-        tokenize' "1 keyword ( 123 )" "" @?= [Num 1, Keyword "keyword", OpenScope, Num 123, CloseScope],
-    testCase "tokenize" $
-        tokenize "1 keyword ( 123 )" @?= [Num 1, Keyword "keyword", OpenScope, Num 123, CloseScope],
-    testCase "tokenize" $
-        tokenize "(define x 2)\n(+ 3 x)" @?= [OpenScope, Keyword "define", Keyword "x", Num 2, CloseScope, OpenScope, Keyword "+", Num 3, Keyword "x", CloseScope]
-  ]
+tokenizeTests :: (String, String, [Token]) -> TestTree
+tokenizeTests (name, input, output) = testCase ("Test Tokenize: " ++ name)  $
+        tokenize input @?= output
+
+loop :: [(String, String, [Token])] -> [TestTree]
+loop [] = []
+loop (x:[]) = [tokenizeTests x]
+loop (x:xs) = (tokenizeTests x: loop xs)
+
+tokenizeSuite :: TestTree
+tokenizeSuite = testGroup "tokenize tests" (loop listTokenizeTests)
 
 tokenSuite :: TestTree
 tokenSuite = testGroup "Parsing.Token Test Suite" [
         parseTokenTests,
-        tokenizeTests
+        tokenizeSuite
     ]
