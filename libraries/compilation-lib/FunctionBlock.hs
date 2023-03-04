af7e24f8ddc274d016ffdaa3394bfea6d907cd03
Leon Sautour <leon@sautour.net>
2023-03-04 22:14:38 +0100

    [Feature] implement Show for FunctionBlock

--- libraries/compilation-lib/FunctionBlock.hs
+++ libraries/compilation-lib/FunctionBlock.hs
@@ -1,6 +1,11 @@
+{-# LANGUAGE InstanceSigs #-}
+
 module FunctionBlock where
 
 import Instruction
 
 data FunctionBlock = FunctionBlock String [Instruction]
 
+instance Show FunctionBlock where
+    show :: FunctionBlock -> String
+    show (FunctionBlock name instructions) = "func " ++ name ++ "\n" ++ printInstructionList instructions ++ "end\n"
