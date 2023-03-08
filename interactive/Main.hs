5b64b301f37920aac6a035f21c3e57d65991e637
Leon Sautour <leon@sautour.net>
2023-03-08 13:07:15 +0100

    [WIP-Not-Building] Non tested, implemented interactive mode

--- interactive/Main.hs
+++ interactive/Main.hs
@@ -1,4 +1,35 @@
 module Main where
 
+import FunctionBlock
+import Utils
+import Parsing.Ast
+import Parsing.Cpt
+import Parsing.Token
+import Compilation.Registry
+import Compilation.Compile
+import Exec
+import Exec.InferType
+
+mainLoop :: [FunctionBlock] -> Registry -> IO ()
+mainLoop fns reg = do
+    -- Get input and parse into AST
+    ast <- parseExprList . parseTokenList . tokenize <$> getInput [] 0
+
+    -- Remove main from functions
+    let withoutMain = removeMainFunc fns
+
+    -- Compile new functions
+    let (compiled, newReg) = compileProgramAddition (ExprList ast) reg
+    let resPgm = head compiled : withoutMain ++ init compiled
+
+    -- Run assembly language
+    _ <- executeFunc resPgm "main" initStack
+
+    -- Recursive Call
+    mainLoop resPgm reg
+
 main :: IO ()
-main = return ()
+main = do
+    putStrLn "*** Welcome to the glados interactive prompt ***"
+
+    mainLoop [] emptyRegistry
